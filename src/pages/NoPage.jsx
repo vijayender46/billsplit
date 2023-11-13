efb327dfca1ddf9d36df2844d7cf74693657cffc
vj146 <121240553+vj146@users.noreply.github.com>
2023-11-13 15:32:09 +0000

    link fix

--- src/pages/NoPage.jsx
+++ src/pages/NoPage.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 export default function NoPage() {
   return (
-    <div>
-        <h1>No Page Found</h1>
-        <Link to='/'>Back to Homepage</Link>
+    <div className='landingPage'>
+        <h1>Please click on Any Scenario Tab above</h1>
     </div>
   )
 }
